b8921e803879ba77698746369a5b014f1f5a34ee
KananValizada <kenan.veli@yahoo.com>
2019-11-12 00:36:18 +0400

    design-options done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 let design =document.getElementById("design-options")
-design.addEventListener("click",changeText)
 let triangle = document.createElement("div")
 let li=document.querySelectorAll(".design-options>li")
-console.log(li)
+let designText = document.querySelectorAll(".design-text")
 triangle.style.cssText=" position:absolute ; bottom:-9px;background-color width: 0; height: 0; border-style: solid; border-width: 10px 10px 0 10px;border-color: #18cfab transparent transparent transparent;"
-
-function changeText(event){
+ for(let i=0;i<li.length;i++){ 
+   li[i].addEventListener("click",changeText)
+   function changeText(){
      for(a of li){
-      a.className=""
-  }
-  if(event.target.className!=="design-toogle"){
-       event.target.className="design-toogle"
-       event.target.append(triangle)
-  }else{
-    event.target.className=""
-  }
- 
-    
-
-
-}
+       a.classList.remove("design-toogle")
+     }
+     for(b of designText){
+       b.classList.add("hidden")
+     }
+     li[i].classList.toggle("design-toogle")
+     li[i].append(triangle)
+     designText[i].classList.remove("hidden")
+   }
+ }
