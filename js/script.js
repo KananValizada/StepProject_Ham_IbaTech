a071ff06c609984ed75563bb6ad7b43dadf007c1
Kanan R. Valizada <KananRv@CODE.LOCAL>
2019-11-18 17:23:31 +0400

    optimized 2

--- js/script.js
+++ js/script.js
@@ -1,53 +1,54 @@
 
 // ***************** OUR CERVICES TAB SECTION **********************  //
-let design =$("#design-options")
-let designLi=$(".design-options>li")
+
+let designLi = $(".design-options>li")
 let designText = $(".design-text")
-for(let i=0;i<designLi.length;i++){
-  designLi.eq(i).attr("dataKey",i)
-  designText.eq(i).attr("dataValue",i)
+for (let i = 0; i < designLi.length; i++) {
+  designLi.eq(i).attr("dataKey", i)
+  designText.eq(i).attr("dataValue", i)
 }
-designLi.click(function(){
+designLi.click(function () {
   designLi.removeClass("design-toggle")
   $(this).addClass('design-toggle');
   let match1 = $('.design-toggle').attr('dataKey');
-designText.hide()
-  $(`.design-text[dataValue="${match1}"]`).css("display","flex");
+  designText.hide()
+  $(`.design-text[dataValue="${match1}"]`).css("display", "flex");
 })
 
 // ***************** OUR AMAZING WORK **********************  //
- let loadMore=$(".load-more")
- let web =$(".web")
- let wordpress =$(".wordpress")
- let landingPage =$(".landing-page")
- let graphicDesign = $(".graphic-design")
- loadMore.on("click",addImages)
-let n=0
- function addImages(){
-   n++
-   for(let i=0;i<5;i++){
-     wordpress.eq(i).show()
-     web.show()
-   }
-   if(n==2){
-     wordpress.show()
-     landingPage.show()
-     loadMore.hide()
-   }
- }
- let amazingImages = $(".amazing-work-images-item")
- let amazingOptionsLi =$(".amazing-work-options>li")
- amazingOptionsLi.on("click",sortImages)
- function sortImages(){
-   amazingImages.hide()
+
+let loadMore = $(".load-more")
+let graphicDesign = $(".graphic-design")
+let amazingImages = $(".amazing-work-images-item")
+loadMore.on("click", addImages)
+let n = 0
+function addImages() {
+  n++
+  for (let i = 0; i < 24; i++) {
+    amazingImages.eq(i).show()
+  }
+  if (n == 2) {
+    amazingImages.show()
+    loadMore.hide()
+  }
+}
+let amazingOptionsLi = $(".amazing-work-options>li")
+amazingOptionsLi.on("click", sortImages)
+function sortImages() {
+  amazingImages.hide()
   amazingOptionsLi.removeClass("option-style")
   $(this).addClass("option-style")
   loadMore.hide()
-  n=0
-  let dataKey=$(this).attr("dataKey")
- dataKey!=undefined ? $(`.${dataKey}`).show() : graphicDesign.show() ;loadMore.css("display","flex")
- }
- 
+  n = 0
+  let dataKey = $(this).attr("dataKey")
+  if (dataKey != undefined) {
+    $(`.${dataKey}`).show()
+  } else {
+    graphicDesign.show();
+    loadMore.css("display", "flex")
+  }
+}
+
 //   ********************  Slick Slide  *************************   //
 
 $('.slider-for').slick({
@@ -61,26 +62,19 @@ $('.slider-nav').slick({
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   centerMode: true,
-  arrows:true,
+  arrows: true,
   focusOnSelect: true,
-
 });
 
-let slickSlide =$(".slider-nav .slick-slide")
-let slickSlideImages= $(".slider-nav .slick-slide img")
-let prevButton =$(".slick-prev")
-let nextButton =$(".slick-next")
-prevButton.on("click",arrowActive)
-nextButton.on("click",arrowActive)
-slickSlide.css("outline","none")
-function arrowActive(){
-slickSlideImages.removeClass("slider-img-active")
-for(let i=0 ;i<slickSlide.length;i++){
-if(slickSlide[i].classList.contains("slick-center")){
-  slickSlideImages[i].classList.add("slider-img-active")}}
+$(".slider-nav .slick-slide").css("outline", "none")
+let slickSlideImages = $(".slider-nav .slick-slide img")
+$(".slick-prev").on("click", arrowActive)
+$(".slick-next").on("click", arrowActive)
+function arrowActive() {
+  slickSlideImages.removeClass("slider-img-active")
+  $(".slick-center div img").addClass("slider-img-active")
 }
-slickSlideImages.on("click",imageImprove)
-  function imageImprove(){
-    slickSlideImages.removeClass("slider-img-active")
-   $(this).addClass("slider-img-active")
-  }
+slickSlideImages.click(function imageImprove() {
+  slickSlideImages.removeClass("slider-img-active")
+  $(this).addClass("slider-img-active")
+})
