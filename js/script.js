1d0ca1b9183ab0d433b2fe4903eea9b1545bf688
Kanan R. Valizada <KananRv@CODE.LOCAL>
2019-11-18 16:25:15 +0400

    Optimizied

--- js/script.js
+++ js/script.js
@@ -1,71 +1,56 @@
-let design =document.getElementById("design-options")
-let triangle = document.createElement("div")
-let li=document.querySelectorAll(".design-options>li")
-let designText = document.querySelectorAll(".design-text")
-triangle.style.cssText=" position:absolute ; bottom:-9px;background-color width: 0; height: 0; border-style: solid; border-width: 10px 10px 0 10px;border-color: #18cfab transparent transparent transparent;"
- for(let i=0;i<li.length;i++){ 
-   li[i].addEventListener("click",changeText)
-   function changeText(){
-     for(a of li){
-       a.classList.remove("design-toogle")
-     }
-     for(b of designText){
-       b.classList.add("hidden")
-     }
-     li[i].classList.toggle("design-toogle")
-     li[i].append(triangle)
-     designText[i].classList.remove("hidden")
-   }
- }
- let loadMore =document.querySelector(".load-more")
- let web =document.querySelectorAll(".web")
- let wordpress =document.querySelectorAll(".wordpress")
- let landingPage =document.querySelectorAll(".landing-page")
- let graphicDesign = document.querySelectorAll(".graphic-design")
- loadMore.addEventListener("click",addImages)
+
+// ***************** OUR CERVICES TAB SECTION **********************  //
+let design =$("#design-options")
+let designLi=$(".design-options>li")
+let designText = $(".design-text")
+for(let i=0;i<designLi.length;i++){
+  designLi.eq(i).attr("dataKey",i)
+  designText.eq(i).attr("dataValue",i)
+}
+designLi.click(function(){
+  designLi.removeClass("design-toggle")
+  $(this).addClass('design-toggle');
+  let match1 = $('.design-toggle').attr('dataKey');
+designText.hide()
+  $(`.design-text[dataValue="${match1}"]`).css("display","flex");
+})
+
+// ***************** OUR AMAZING WORK **********************  //
+ let loadMore=$(".load-more")
+ let web =$(".web")
+ let wordpress =$(".wordpress")
+ let landingPage =$(".landing-page")
+ let graphicDesign = $(".graphic-design")
+ loadMore.on("click",addImages)
 let n=0
  function addImages(){
    n++
-   console.log(n)
-   web.forEach((x)=>{
-     x.style.display="block"
-   })
    for(let i=0;i<5;i++){
-     wordpress[i].style.display="block"
+     wordpress.eq(i).show()
+     web.show()
    }
    if(n==2){
-     wordpress.forEach(x=>x.style.display="block")
-     landingPage.forEach(x=>x.style.display="block")
-     loadMore.style.display ="none"
+     wordpress.show()
+     landingPage.show()
+     loadMore.hide()
    }
  }
- let amazingOptions = document.querySelector(".amazing-work-options")
- let amazingOptionsLi =document.querySelectorAll(".amazing-work-options>li")
- amazingOptionsLi.forEach(x=>x.addEventListener("click",sortImages))
- function sortImages(event){
-   graphicDesign.forEach(x=>x.style.display="none")
-  landingPage.forEach(x=>x.style.display="none")
-  web.forEach(x=>x.style.display="none")
-  wordpress.forEach(x=>x.style.display="none")
-  amazingOptionsLi.forEach(x=>x.classList.remove("option-style"))
-  event.target.classList.add("option-style")
-  loadMore.style.display ="none"
+ let amazingImages = $(".amazing-work-images-item")
+ let amazingOptionsLi =$(".amazing-work-options>li")
+ amazingOptionsLi.on("click",sortImages)
+ function sortImages(){
+   amazingImages.hide()
+  amazingOptionsLi.removeClass("option-style")
+  $(this).addClass("option-style")
+  loadMore.hide()
   n=0
-if(event.target.innerText=="Graphic Design"){
-  graphicDesign.forEach(x=>x.style.display="block")
-}else if(event.target.innerText=="Web Design"){
-  web.forEach(x=>x.style.display="block")
-}else if(event.target.innerText=="Landing Pages"){
-  landingPage.forEach(x=>x.style.display="block")
-}else if(event.target.innerText=="Wordpress"){
-  wordpress.forEach(x=>x.style.display="block")
-}else {
-  graphicDesign.forEach(x=>x.style.display="block")
-  loadMore.style.display ="flex"
-}
+  let dataKey=$(this).attr("dataKey")
+ dataKey!=undefined ? $(`.${dataKey}`).show() : graphicDesign.show() ;loadMore.css("display","flex")
  }
  
- $('.slider-for').slick({
+//   ********************  Slick Slide  *************************   //
+
+$('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
@@ -81,28 +66,21 @@ $('.slider-nav').slick({
 
 });
 
-//   ********************  Slick Slide  *************************   //
-
-
-let slickSlide =document.querySelectorAll(".slider-nav .slick-slide")
-let slickSlideImages= document.querySelectorAll(".slider-nav .slick-slide img")
-let prevButton =document.querySelector(".slick-prev")
-let nextButton =document.querySelector(".slick-next")
-prevButton.addEventListener("click",arrowActive)
-nextButton.addEventListener("click",arrowActive)
-slickSlide.forEach((x)=>{
-  x.style.cssText="outline: none;"
-})
+let slickSlide =$(".slider-nav .slick-slide")
+let slickSlideImages= $(".slider-nav .slick-slide img")
+let prevButton =$(".slick-prev")
+let nextButton =$(".slick-next")
+prevButton.on("click",arrowActive)
+nextButton.on("click",arrowActive)
+slickSlide.css("outline","none")
 function arrowActive(){
-slickSlideImages.forEach(x=>x.classList.remove("slider-img-active"))
+slickSlideImages.removeClass("slider-img-active")
 for(let i=0 ;i<slickSlide.length;i++){
 if(slickSlide[i].classList.contains("slick-center")){
   slickSlideImages[i].classList.add("slider-img-active")}}
 }
-slickSlideImages.forEach(x=>{
-  x.addEventListener("click",imageImprove)
+slickSlideImages.on("click",imageImprove)
   function imageImprove(){
-    slickSlideImages.forEach(x=>x.classList.remove("slider-img-active"))
-    x.classList.add("slider-img-active")
+    slickSlideImages.removeClass("slider-img-active")
+   $(this).addClass("slider-img-active")
   }
-})
